691272cc507190cc9d0fe818dfee78122f0b55ff
maria-balwi <patricereyes.MPR@gmail.com>
2026-07-13 17:07:39 +0800

    style: added topbar on the layout of each module

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import MobileBlock from './MobileBlock'
+import Topbar from "./Topbar";
 
 export default function Layout() {
   return (
     <MobileBlock>
         <Sidebar />
-        <main className="flex-1 bg-[#EFF1F4] overflow-y-auto p-8">
-          <Outlet />
-        </main>
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Topbar />
+          <main className="flex-1 bg-[#EFF1F4] overflow-y-auto p-6">
+            <Outlet />
+          </main>
+        </div>
     </MobileBlock>
   )
 }
